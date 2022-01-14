57a6fa56835c292822fb9cba202ea545cdbcb90c
fariesfahrudin <97731640+fariesfahrudin@users.noreply.github.com>
2022-01-14 18:09:42 +0700

    Tugas UAS

---
+++ data diri.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	char nama[25];
+	int nim;
+	char prodi[50];
+	
+	printf("\nFARIES FAHRUDIN",nama);
+	printf("\nProdi Teknik Informatika",prodi);
+	printf("\n07.1.2021.0009",nim);
+	
+	return 0;
+}
