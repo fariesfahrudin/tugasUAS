521105944cbc252929a6acfc6ebc49913e22589e
fariesfahrudin <97731640+fariesfahrudin@users.noreply.github.com>
2022-02-25 15:13:52 +0700

    Add files via upload

---
+++ array 1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int nilai[3] = { 8, 9, 7};
+	string nama[3];
+	nama[0] = "NAMA : FARIES FAHRUDIN \n";
+	nama[1] = "NIM  : 07.1.2021.0009\n";
+	nama[2] = "Prodi: TEKNIK INFORMATIKA\n";
+	cout<<nama[0]<<nama[1]<<nama[2]<<endl;
+	cout<<nilai[3];
+}
